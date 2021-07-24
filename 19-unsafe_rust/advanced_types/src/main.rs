b62b0721be514fa7259c3d27f30bfd0461f6cf51
dssgabriel <dss.gabriel@protonmail.com>
2021-07-25 00:59:54 +0200

    Continued chapter 19 - Advanced traits and types

---
+++ 19-unsafe_rust/advanced_types/src/main.rs
@@ -0,0 +1,23 @@
+type Kilometers = i32;
+
+type Thunk = Box<dyn Fn() + Send + 'static>;
+
+fn takes_long_type(f: Thunk) {
+    // --snip--
+}
+
+fn returns_long_type() -> Thunk {
+    // --snip--
+}
+
+fn bar() -> ! {
+    // --snip--
+}
+
+fn main() {
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+    println!("x + y = {}", x + y);
+
+    let f: Thunk = Box::new(|| println!("hi"));
+}
